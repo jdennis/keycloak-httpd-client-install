205527433d2a9e2a06ae981ac8e81be997637893
John Dennis <jdennis@redhat.com>
2016-02-27 09:57:00 -0500

    Add license, RPM spec file, improve packaging

--- setup.py
+++ setup.py
@@ -2,15 +2,25 @@
 
 from distutils.core import setup
 
-setup(name='keycloak',
+name = 'keycloak-httpd-client'
+
+setup(name=name,
       version='0.0',
-      description='Python tools to setup and manage Keycloak',
+      description='Tools to configure Apache HTTPD as Keycloak client',
       author='John Dennis',
       author_email='jdennis@redhat.com',
+      license='GPLv3',
       packages = ['keycloak'],
       scripts = ['bin/keycloak-client-install'],
-      data_files = [('/usr/share/python-keycloak/templates',
+      data_files = [('/usr/share/python-{name}/templates'.format(name=name),
                      ['templates/mellon_httpd.conf',
                       'templates/sp_metadata.tpl'])],
-      requires = ['requests', 'requests-oauthlib', 'jinja2'],
+      requires = ['requests', 'requests_oauthlib', 'jinja2'],
+      classifiers = [           # see https://pypi.python.org/pypi?%3Aaction=list_classifiers
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Development Status :: 3 - Alpha",
+          "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+          "Operating System :: POSIX",
+      ],
      )
